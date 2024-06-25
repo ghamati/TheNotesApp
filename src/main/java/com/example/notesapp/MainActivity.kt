c4d7936c8a5cbea267f5a676df5d5d8e2b65f224
ghamati <qamati1234@gmail.com>
2024-06-25 20:18:57 +0330

    last2 try

--- src/main/java/com/example/notesapp/MainActivity.kt
+++ src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.notesapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityMainBinding
-    private lateinit var db : NoteDatabase
+    private lateinit var db: NoteDatabase
     private lateinit var notesAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +22,40 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        val switch = findViewById<SwitchCompat>(R.id.switchmode)
+
+        val sharedPreferences = getSharedPreferences("Mode",Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val nightMode = sharedPreferences.getBoolean("night",false)
+
+        if (nightMode){
+            switch.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night",false)
+                editor.apply()
+
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night",true)
+                editor.apply()
+
+            }
+        }
+
+
         db = NoteDatabase(this)
-        notesAdapter = NoteAdapter(db.getAllNotes(),this)
+        notesAdapter = NoteAdapter(db.getAllNotes(), this)
 
         binding.noteView.layoutManager = LinearLayoutManager(this)
         binding.noteView.adapter = notesAdapter
 
         binding.addButton.setOnClickListener {
-            val intent = Intent(this,AddNoteActivity::class.java)
+            val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
     }
@@ -38,3 +65,6 @@ class MainActivity : AppCompatActivity() {
         notesAdapter.refreshData(db.getAllNotes())
     }
 }
+
+
+
